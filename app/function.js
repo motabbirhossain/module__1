a463b17747e7fb25dfb37da2279a22ba73599e8f
motabbirhossain <rajuahmed362002@gmail.com>
2023-09-10 14:05:21 +0600

    module js start

---
+++ app/function.js
@@ -0,0 +1,9 @@
+
+
+
+const ageCal = (name , age)=>{
+    return `Hi ${name} , You are ${age} years Old`
+};
+
+// This works
+module.exports = ageCal;
---
+++ index.js
@@ -0,0 +1,4 @@
+
+const ageCal = require("./app/function.js");
+
+console.log(ageCal("Raju", 120));
